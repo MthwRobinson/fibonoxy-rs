37db4112e936c0ded13af1b4054d3ba63f08b079
Matt Robinson <mthw.wm.robinson@gmail.com>
2024-10-21 13:58:56 -0400

    add emoji module

---
+++ src/emoji.rs
@@ -0,0 +1,34 @@
+#[derive(PartialEq, Debug)]
+enum Emoji {
+    Ox,
+    Camel,
+    Unknown,
+}
+
+fn name_to_emoji(name: &str) -> Emoji {
+    if name == "ox" {
+        Emoji::Ox
+    } else if name == "camel" {
+        Emoji::Camel
+    } else {
+        Emoji::Unknown
+    }
+}
+
+fn emoji_code(emoji: Emoji) -> String {
+    match emoji {
+        Emoji::Ox => String::from("\u{1F402}"),
+        Emoji::Camel => String::from("\u{1F42A}"),
+        _ => String::from("\u{1F937}"),
+    }
+}
+
+#[cfg(test)]
+mod emoji_tests {
+    use super::*;
+
+    #[test]
+    fn name_to_emoji_works() {
+        assert_eq!(name_to_emoji("ox"), Emoji::Ox)
+    }
+}
